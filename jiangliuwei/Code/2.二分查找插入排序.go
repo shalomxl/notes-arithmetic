582a2905f35dc00da8caa39744fddc3f830376aa
nicholas <nicholas_liuwei@163.com>
2019-05-28 00:03:35 +0800

    再次优化二分查找插入排序

--- jiangliuwei/Code/2.二分查找插入排序.go
+++ jiangliuwei/Code/2.二分查找插入排序.go
@@ -41,3 +41,50 @@ func BinarySearchIndex(arr []int, maxIndex int, data int) int {
 	}
 	return begin
 }
+package main
+
+import "fmt"
+
+//func main() {
+//	arr := []int{20, 15, 8, 61, 79, 26, 8, 78, 56, 79}
+//
+//	for i := 1; i < len(arr)-1; i++ {
+//		temp:=arr[i]
+//		index := BinarySearchIndex(arr[:i], i-1, arr[i])
+//
+//		j:=i-1
+//		for {
+//
+//			arr[j+1]=arr[j]
+//			if j==index{
+//				arr[index]=temp
+//				break
+//			}
+//			j--
+//
+//		}
+//		fmt.Println("====>>",arr)
+//
+//	}
+//	fmt.Println(arr)
+//
+//}
+
+//func BinarySearchIndex(arr []int,end int,data int) int{
+//
+//	begin:=0
+//	for {
+//		if begin > end {
+//			break
+//		}
+//		mid := (begin + end) / 2
+//		if arr[mid]>data{
+//			end=mid-1
+//		}else {
+//			begin=mid+1
+//		}
+//
+//	}
+//	return begin
+//
+//}
